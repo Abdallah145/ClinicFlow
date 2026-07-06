f080200f902cffa91e4026f1f0c383239860e3b8
Kerolos-Wagdy1122 <123576550+Kerolos-Wagdy1122@users.noreply.github.com>
2026-07-06 19:05:58 +0300

    created doctor dashboard logic only in DoctorDashboard.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,29 +12,15 @@ import MyAppointments from "./page/MyAppointments.jsx";
 import Appointment from "./page/Appointment.jsx";
 import Footer from "./components/Footer.jsx";
 import 'react-toastify/dist/ReactToastify.css';
+import { AuthProvider, useAuth } from "./context/AuthContext.jsx";
+import DoctorDashboard from "./views/DoctorDashboard";
 
 
 const App = () => {
   return (
-    <div>
-      <div className="mx-4 sm:mx-[10%]">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/doctors" element={<Doctors />} />
-          <Route path="/doctors/:speciality" element={<Doctors />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/my-profile" element={<MyProfile />} />
-          <Route path="/my-appointment" element={<MyAppointments />} />
-          <Route path="/appointment/:docId" element={<Appointment />} />
-        </Routes>
-      </div>
-      <div className="bg-gray-100 px-4 sm:py-[0.1%] sm:px-[10%]">
-        <Footer  />
-      </div>
-    </div>
+    <AuthProvider>
+      <DoctorDashboard />
+    </AuthProvider>
   );
 };
 
